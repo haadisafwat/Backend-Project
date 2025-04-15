c668459f0c7efdb1674a94c764dab9cc34a8ca65
haadisafwat <safwathaadi@gmail.com>
2025-04-15 15:07:15 +0100

    completed all table and value creation

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -10,8 +10,41 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
       img_url VARCHAR(1000)
       )
       ;`)
-
-
+  })
+  .then(() => {
+    return db.query(`
+      CREATE TABLE users (
+      username VARCHAR(50) PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      avatar_url VARCHAR(1000)
+      )
+      ;`)
+  })
+  .then(() => {
+    return db.query(`
+      CREATE TABLE articles (
+      article_id SERIAL PRIMARY KEY,
+      title VARCHAR(255) NOT NULL,
+      topic VARCHAR(255) REFERENCES topics(slug),
+      author VARCHAR(255) REFERENCES users(username),
+      body TEXT NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      votes INT DEFAULT 0,
+      article_img_url VARCHAR(1000)
+      )
+      ;`)
+  })
+  .then(() => {
+    return db.query(`
+      CREATE TABLE comments (
+      comment_id SERIAL PRIMARY KEY,
+      article_id INT NOT NULL REFERENCES articles(article_id),
+      body TEXT NOT NULL,
+      author VARCHAR(255) REFERENCES users(username),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      votes INT DEFAULT 0
+      )
+      ;`)
   })
 
 
